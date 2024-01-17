e04ddaa9aa60442e5bb286eb74f1931d22b3c03e
Samojit Paul <paulsamo1234@gmail.com>
2024-01-17 15:12:54 +0530

    Util code discussion

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@
 import dotenv from "dotenv";
 import dbConnection from "./db/connection.js";
 
+import app from "./app.js";
+
 dotenv.config({
     path: './env'
 });
 
-dbConnection();
+dbConnection()
+.then(() =>{
+
+    app.on("error",(err) =>{
+        console.log("Error: " + err);
+        throw err;
+    });
+
+    app.listen(process.env.PORT || 8000 , () =>{
+        console.log(`Server running at ${process.env.PORT}`);
+    });
+})
+.catch((err) => {
+    console.error("Mongo Db Connection Failed", err.message);
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+
+//using Promise
+const asyncHandler = (requestHandler) =>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((err) => next(err));
+    }   
+}
+
+export default asyncHandler;
+
+
+
+//Using try/catch
+
+// const asyncHandler = (fn) => async(req,res,next) => {
+//     try {
+        
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message:error.message
+//         });
+//     }
+// };
